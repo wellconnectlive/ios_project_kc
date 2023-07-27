050639d21f7a48d5699a0b481c93adfef69e41b4
cookyourweb <hello.cookyourweb@gmail.com>
2023-07-27 18:19:33 +0200

    added firebase configuration

--- WellConnectLive/WellConnectLive/WellConnectLiveApp.swift
+++ WellConnectLive/WellConnectLive/WellConnectLiveApp.swift
@@ -6,16 +6,32 @@
 //
 
 import SwiftUI
+import FirebaseCore
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
 
+    return true
+  }
+}
 
 @main
 struct wellnesstrackApp: App {
+    
+    // register app delegate for Firebase setup
+      @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    
     let persistenceController = PersistenceController.shared
     //Para observar cualquier cambio que reciba appState sea observado y actualizar la vista
     @StateObject private var appState = AppState()
 
     var body: some Scene {
         WindowGroup {
+            NavigationView {
+                    ContentView()
+                  }
             //Accedemos a la clase de navegacion para movernos de vista según el appState que tengamos
             MainStateNavigation()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
